528518373319ff1e77d45427f3a10731e34d58a2
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-24 19:48:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,16 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library (path)
+  emoticons = YAML.load_file(path)
+  # p emoticons
+  # p "==============="
+  emohash ={"get_meaning" => {}, "get_emoticon" => {}}
+  emoticons.each do |word, emos|
+    emos1, emos2 = icons
+    emohash["getting_meaning"] => :emos2 = word
+    emohash["get_emoticon"] => :emo1 = emos2
+  end
+  emohash
 end
 
 def get_japanese_emoticon
