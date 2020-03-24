6703b758a1a37bd5cdebeff30825d0db1368095f
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-24 20:12:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,13 +24,13 @@ end
 # end
 
 def get_japanese_emoticon(path, emoticon)
-  emoticons = load_library(path) #call load_library
-  result = emoticons["get_emoticon"][emoticon]
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
+emoticons = load_library(path)
+result = emoticons["get_emoticons"][emoticon]
+if result
+  result
+else
+  "Sorry, that emoticon was not found"
+end
 end
 
 def get_english_meaning
